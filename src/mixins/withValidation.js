9c0624f3f773694057b71c762341093485601599
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-19 00:11:26 +0700

    add validation messages

--- src/mixins/withValidation.js
+++ src/mixins/withValidation.js
@@ -1,3 +1,16 @@
+const errorLabels = {
+  en: {
+    required: 'Is required',
+    minLength: 'Is too short',
+    maxLength: 'Is too long',
+    alphaNum: 'Is invalid',
+    includedIn: 'Is not in the list',
+    jsonValid: 'Cannot parse it',
+    validEvents: 'Events are invalid',
+    withEvents: 'Needs at least one event'
+  }
+}
+
 export default {
   computed: {
     errors () {
@@ -21,6 +34,16 @@ export default {
       return Object
         .keys(this.$v[key].$params)
         .filter(type => !this.$v[key][type])
+        .map(type => {
+          const { messages, locale } = this.$i18n
+          const localizedMessages = messages[locale]
+          const errorKeyType = ((localizedMessages.errors || {})[key] || {})[type]
+          const errorType = (localizedMessages.errors || {})[type]
+          if (errorKeyType) { return errorKeyType }
+          if (errorType) { return errorType }
+          if (errorLabels[locale][type]) { return errorLabels[locale][type] }
+          return type
+        })
     }
   }
 }
