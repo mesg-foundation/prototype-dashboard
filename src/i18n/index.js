0c0521ae980fc1c421d8cfee824a57c29965f703
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-18 14:31:14 +0700

    add timeago lib

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import VueTimeago from 'vue-timeago'
 import dateTimeFormats from './dateTimeFormats'
 import numberFormats from './numberFormats'
+
+Vue.use(VueTimeago, {
+  name: 'timeago',
+  locale: 'en-US',
+  locales: {
+    'en-US': require('vue-timeago/locales/en-US.json')
+  }
+})
 
 Vue.use(VueI18n)
 
