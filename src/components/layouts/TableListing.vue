142fa0d977771c63fef8ab0f457b78e9a5b98a00
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-18 14:31:52 +0700

    add table listing component

---
+++ src/components/layouts/TableListing.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card flat>
+    <v-toolbar card class="grey lighten-3">
+      <v-toolbar-title class="headline">{{ title }}</v-toolbar-title>
+      <slot name="toolbar"></slot>
+    </v-toolbar>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      hide-actions>
+      <template slot="items" scope="props">
+        <slot v-bind="props.item"></slot>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      headers: {
+        type: Array,
+        required: true
+      },
+      items: {
+        type: Array,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import "../../variables"
+
+  nav.grey.lighten-3 + .table__overflow thead {
+    background: $grey.lighten-3;
+  }
+</style>
