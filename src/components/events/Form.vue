8cd6d9145139a8cd018f3111bb21200bdab5b393
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-20 22:45:13 +0700

    add event form component

---
+++ src/components/events/Form.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-card flat>
+    <form @submit.prevent="submit()">
+      <v-toolbar card class="secondary">
+        <v-toolbar-title class="headline">{{ $t('title') }}</v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <WebhookDetailList
+        :webhook="webhook">
+      </WebhookDetailList>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-text-field
+          :label="$t('labels.payload')"
+          :error-messages="errors.payload"
+          v-model="payload"
+          @input="$v.payload.$touch()"
+          multi-line
+          :hint="$t('hint')"
+          persistent-hint
+          hide-details
+          :rows="6"
+          required>
+        </v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          primary dark block
+          :disabled="!isValid"
+          type="submit">
+          {{ $t('submit') }}
+        </v-btn>
+      </v-card-actions>
+    </form>
+  </v-card>
+</template>
+
+<i18n>
+  en:
+    title: "Test the webhook"
+    submit: "Send test event"
+    hint: "A JSON with the data from your Ethereum contract's event"
+    labels:
+      payload: "Parameters"
+</i18n>
+
+<script>
+  import withValidation from '@/mixins/withValidation'
+  import { mapActions } from 'vuex'
+  import { required, jsonValid } from '@/validators'
+  import WebhookDetailList from '@/components/webhooks/DetailList.vue'
+  export default {
+    components: {
+      WebhookDetailList
+    },
+    mixins: [
+      withValidation
+    ],
+    props: {
+      webhook: {
+        type: Object,
+        required: true
+      }
+    },
+    data () {
+      return {
+        payload: null
+      }
+    },
+    validations: {
+      payload: {
+        required,
+        jsonValid
+      }
+    },
+    methods: {
+      ...mapActions({
+        createEvent: 'events/create'
+      }),
+      submit () {
+        this.createEvent({
+          payload: this.payload
+        })
+          .then(event => this.$emit('saved', event))
+      }
+    }
+  }
+</script>
