ec146f84ba78822c293d5990f7df8c5c4de902ab
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-24 18:13:13 +0700

    add project edition

---
+++ src/components/projects/Create.vue
@@ -0,0 +1,26 @@
+<template>
+  <ProjectForm
+    @saved="projectSaved">
+  </ProjectForm>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  import ProjectForm from '@/components/projects/Form.vue'
+  export default {
+    components: {
+      ProjectForm
+    },
+    methods: {
+      ...mapActions({
+        changeProject: 'session/changeProject'
+      }),
+      projectSaved (project) {
+        this.changeProject(project)
+        .then(_ => {
+          this.$router.push({ name: 'Webhooks' })
+        })
+      }
+    }
+  }
+</script>
---
+++ src/components/projects/Form.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card flat>
+    <form @submit.prevent="submit()">
+      <v-toolbar card class="secondary">
+        <v-toolbar-title class="headline">{{ $t('title') }}</v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-text-field
+          :label="$t('labels.name')"
+          :error-messages="errors.name"
+          v-model="name"
+          @input="$v.name.$touch()"
+          autofocus
+          required>
+        </v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          primary dark
+          :disabled="!isValid"
+          type="submit">
+          {{ $t('submit') }}
+        </v-btn>
+      </v-card-actions>
+    </form>
+  </v-card>
+</template>
+
+<i18n>
+  en:
+    title: "Create my project"
+    submit: "Save this project"
+    labels:
+      name: "Name"
+</i18n>
+
+<script>
+  import withValidation from '@/mixins/withValidation'
+  import { mapActions } from 'vuex'
+  import { required, alphaNum } from '@/validators'
+  export default {
+    mixins: [
+      withValidation
+    ],
+    data () {
+      return {
+        name: 'Test project'
+      }
+    },
+    validations: {
+      name: {
+        required,
+        alphaNum
+      }
+    },
+    methods: {
+      ...mapActions({
+        createProject: 'projects/create'
+      }),
+      submit () {
+        this.createProject({
+          name: this.name
+        })
+          .then(project => this.$emit('saved', project))
+      }
+    }
+  }
+</script>
