b010e51d2a63f5a51e3ea409c1b88dbc6dab4f80
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-31 16:50:27 +0800

    add billing page

--- src/components/settings/Billing.vue
+++ src/components/settings/Billing.vue
@@ -1,3 +1,48 @@
 <template>
-  <div>billing</div>
+  <v-card flat>
+    <v-card-title class="headline">
+      {{ $t('title', { from, to }) }}
+    </v-card-title>
+    <v-card-text>
+      <label>
+        {{ $t('labels.requests') }}
+        ({{ this.currentProject.requestsCount }} / {{ this.currentProject.requestsMax }})
+      </label>
+      <v-progress-linear
+        :size="100"
+        :width="15"
+        :rotate="-90"
+        :value="percent"
+        class="primary--text">
+      </v-progress-linear>
+    </v-card-text>
+  </v-card>
 </template>
+
+<i18n>
+  en:
+    title: "Usage ({from} - {to})"
+    labels:
+      requests: "Requests number"
+</i18n>
+
+<script>
+  import withCurrentProject from '@/mixins/withCurrentProject'
+  export default {
+    mixins: [withCurrentProject],
+    computed: {
+      percent () {
+        return this.currentProject.requestsCount / this.currentProject.requestsMax * 100
+      },
+      today () {
+        return new Date()
+      },
+      from () {
+        return new Date(this.today.getFullYear(), this.today.getMonth()).toLocaleDateString()
+      },
+      to () {
+        return new Date(this.today.getFullYear(), this.today.getMonth() + 1, 0).toLocaleDateString()
+      }
+    }
+  }
+</script>
