bb34138b449463b34423a71c8b377f778a0b1859
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-07-31 16:50:11 +0800

    add team listing

--- src/components/settings/Team.vue
+++ src/components/settings/Team.vue
@@ -1,3 +1,58 @@
 <template>
-  <div>team</div>
-</template>
+  <table-listing
+    :headers="headers"
+    :items="users"
+    :title="$t('title')"
+    :loading="loadingUsers"
+    transparent>
+    <template scope="user">
+      <td>{{ user.email }}</td>
+      <td>{{ $t(`role.${role(user)}`) }}</td>
+      <td class="text-xs-right">
+        <timeago :since="user.createdAt" :auto-update="10"></timeago>
+      </td>
+    </template>
+  </table-listing></template>
+
+<i18n>
+  en:
+    title: "Team"
+    header:
+      email: "Email"
+      role: "Role"
+      createdAt: "Created at"
+    role:
+      admin: "Admin"
+      other: "Developper"
+</i18n>
+
+<script>
+  import collection from '@/mixins/collection'
+  import withCurrentProject from '@/mixins/withCurrentProject'
+  import TableListing from '@/components/layouts/TableListing.vue'
+  export default {
+    components: {
+      TableListing
+    },
+    mixins: [
+      withCurrentProject,
+      collection('users', component => ({
+        projectId: component.currentProjectId
+      }))
+    ],
+    computed: {
+      headers () {
+        return [
+          { text: this.$t('header.email'), align: 'left', sortable: false },
+          { text: this.$t('header.role'), align: 'left', sortable: false },
+          { text: this.$t('header.createdAt'), align: 'right', sortable: false }
+        ]
+      }
+    },
+    methods: {
+      role (user) {
+        return user.id === this.currentProject.creator.id ? 'admin' : 'other'
+      }
+    }
+  }
+</script>
