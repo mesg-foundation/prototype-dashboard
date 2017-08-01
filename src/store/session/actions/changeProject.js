79f3cb53eec2a729da5fab728abd1de2fa2a1dec
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-08-02 00:33:00 +0800

    add subscriptions when switch project

--- src/store/session/actions/changeProject.js
+++ src/store/session/actions/changeProject.js
@@ -1,5 +1,12 @@
 export default ({ commit, dispatch, rootGetters }, { project }) => {
   // TODO update user to save his last selected project
-  commit('setCurrentProjectId', project.id)
-  dispatch('webhooks/subscribes', {}, { root: true })
+  const projectId = project.id
+  commit('setCurrentProjectId', projectId)
+  return Promise.all([
+    dispatch('events/subscribes', { projectId }, { root: true }),
+    dispatch('contracts/subscribes', { projectId }, { root: true }),
+    dispatch('users/subscribes', { projectId }, { root: true }),
+    dispatch('webhookResults/subscribes', { projectId }, { root: true }),
+    dispatch('webhooks/subscribes', { projectId }, { root: true })
+  ])
 }
