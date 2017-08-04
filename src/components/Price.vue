d94ebb5785714564497614b140ae80ba6d3e4b10
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-08-04 18:30:41 +0800

    add price component

---
+++ src/components/Price.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>{{ $n(price, currency) }}</span>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: Number,
+        required: true
+      },
+      currency: {
+        type: String,
+        default: 'USD'
+      }
+    },
+    computed: {
+      price () { return this.value / 100 }
+    }
+  }
+</script>
