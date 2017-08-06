832214476e941b1e603e42104311c45ed2f095aa
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-08-06 23:39:20 +0700

    add url validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,10 +1,11 @@
-import { email, required, minLength, maxLength, alphaNum } from 'vuelidate/lib/validators'
+import { url, email, required, minLength, maxLength, alphaNum } from 'vuelidate/lib/validators'
 import includedIn from './includedIn'
 import jsonValid from './jsonValid'
 import validEvents from './validEvents'
 import withEvents from './withEvents'
 
 export {
+  url,
   email,
   required,
   minLength,
