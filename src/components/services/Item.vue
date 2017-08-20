1f87ff73eedee024a8f2d83eedcd49590b649c71
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-08-20 16:41:22 +0700

    add service card

---
+++ src/components/services/Item.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card
+    :class="{ 'blue': isSelected, 'lighten-5': isSelected }">
+    <v-card-title class="headline">
+      {{ service.name }}
+    </v-card-title>
+    <v-card-text style="min-height:150px" v-html="service.description"></v-card-text>
+    <v-card-actions>
+      <v-btn block flat primary @click.stop="select">
+        <v-icon
+          v-if="isSelected"
+          class="success--text mr-2">
+          check
+        </v-icon>
+        <span v-if="isSelected">{{ $t('selected') }}</span>
+        <span v-else>{{ $t('select') }}</span>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<i18n>
+  en:
+    select: "Connect this service"
+    selected: "Selected"
+</i18n>
+
+<script>
+  import input from '@/mixins/input'
+  export default {
+    mixins: [
+      input
+    ],
+    props: {
+      service: {
+        type: Object,
+        required: true
+      }
+    },
+    computed: {
+      isSelected () {
+        return this.value === this.service.id
+      }
+    },
+    methods: {
+      select () {
+        this.$emit('input', this.service.id)
+      }
+    }
+  }
+</script>
