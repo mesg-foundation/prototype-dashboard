f5d76a88f17175391fd88d6f12758dcec496076f
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-08 22:32:05 +0700

    do not create specific validations

--- src/components/jsonSchemaForm/mixins/field-extractor.js
+++ src/components/jsonSchemaForm/mixins/field-extractor.js
@@ -15,11 +15,7 @@ export default {
           value: this.value[key] || schemaDefinition.default || '',
           onUpdate: this.onUpdate(key),
           values: schemaDefinition.enum || [],
-          validations: [
-            schema.required.indexOf(key) >= 0 || schemaDefinition.required ? x => !!x || `${key} is required` : null,
-            schemaDefinition.pattern ? x => new RegExp(schemaDefinition.pattern).test(x) || `${key} doesn't match "${schemaDefinition.pattern}"` : null,
-            schemaDefinition.enum ? x => schemaDefinition.enum.indexOf(x) >= 0 || `${key} doesn't have a valid value: ${schemaDefinition.enum.map(e => `"${e}"`).join(', ')}` : null
-          ].filter(x => x)
+          validations: []
         }
       })
     }
