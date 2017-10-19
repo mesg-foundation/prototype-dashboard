7ed656a1c38b7da86de02ea52666de4bc5492673
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-19 12:16:03 +0700

    add erc20 connector and rename kovan to testnet

--- config/prod.env.js
+++ config/prod.env.js
@@ -12,6 +12,7 @@ module.exports = {
   CONNECTORS: JSON.stringify([
     { id: 'ETHEREUM_CONTRACT', fieldName: 'ethereumContract', name: 'Ethereum Contract', picture: 'https://www.ethereum.org/images/logos/ETHEREUM-ICON_Black.png', description: 'Execute an action in Real Time every time this event is triggers from your contract' },
     { id: 'ETHEREUM_TRANSACTION', fieldName: 'ethereumTransaction', name: 'Ethereum Transaction', picture: 'https://www.ethereum.org/images/logos/ETHEREUM-ICON_Black.png', description: 'Execute an action in Real Time every a transaction happen on an Ethereum address' },
-    { id: 'BITCOIN_TRANSACTION', fieldName: 'bitcoinTransaction', name: 'Bitcoin Transaction', picture: 'https://bitcoin.org/img/icons/opengraph.png', description: 'Execute an action in Real Time every time you receive a transaction on Bitcoin' }
+    { id: 'ETHEREUM_ERC20', fieldName: 'ethereumErc20', name: 'ERC20', picture: 'https://cdn-images-1.medium.com/max/1200/1*FD2cH_XiSw8oIlT2ojfPdQ.png', description: 'Execute an action in Real Time every time an ERC20 token emits an event' },
+    // { id: 'BITCOIN_TRANSACTION', fieldName: 'bitcoinTransaction', name: 'Bitcoin Transaction', picture: 'https://bitcoin.org/img/icons/opengraph.png', description: 'Execute an action in Real Time every time you receive a transaction on Bitcoin' }
   ])
 }
