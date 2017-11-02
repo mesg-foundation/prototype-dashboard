d5b402cc925935a7749fd0592390dd549ad017c1
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-11-02 13:43:07 +0630

    add events list graph on the dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,19 +3,64 @@
     <v-toolbar card class="secondary">
       <MenuToggle></MenuToggle>
       <v-toolbar-title class="headline">
-        {{ title }}
+        Dashboard
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
     <v-divider></v-divider>
+    <v-card-text>
+      <EventChart
+        v-if="data"
+        :height="100"
+        :data="data"></EventChart>
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
 import MenuToggle from '@/components/MenuToggle'
+import EventChart from '@/components/charts/events'
+import client from '@/graphql'
+import allEvents from '@/graphql/events/queries/stats.graphql'
+import withCurrentProject from '@/mixins/withCurrentProject'
+import groupDataByTime from '@/utils/groupDataByTime'
+
 export default {
   components: {
-    MenuToggle
+    MenuToggle,
+    EventChart
+  },
+  mixins: [withCurrentProject],
+  data () {
+    return {
+      data: null
+    }
+  },
+  computed: {
+    from () {
+      const now = new Date()
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate() - 14)
+    },
+    to () {
+      const now = new Date()
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    }
+  },
+  mounted () {
+    client().query({
+      query: allEvents,
+      variables: {
+        projectId: this.currentProjectId,
+        from: this.from,
+        to: this.to
+      }
+    })
+      .then(({ data }) => {
+        this.data = groupDataByTime(data.allEvents, {
+          from: this.from,
+          to: this.to
+        })
+      })
   }
 }
 </script>
