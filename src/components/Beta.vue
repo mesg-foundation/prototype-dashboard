808ca93c5d0d367bdd2d113d153cb44b803cbe92
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-11-07 16:58:33 +0700

    fix beta component

--- src/components/Beta.vue
+++ src/components/Beta.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-card>
-    <v-card-title>BETA</v-card-title>
-    <v-divider></v-divider>
-    <v-card-text>
-      <p>We are actually in private beta. If you want to have access to it, please send us an email to
-      <a href="mailto:contact@etherstellar.io">contact@etherstellar.io</a> with the email address you
-      used to signup.</p>
-      <p>Thank you</p>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-        color="success" dark
-        href="mailto:contact@etherstellar.io">
-        Send us an email
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-layout justify-center align-center>
+    <v-card>
+      <v-card-title>BETA</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <p>We are actually in private beta. If you want to have access to it, please send us an email to
+        <a href="mailto:contact@etherstellar.io">contact@etherstellar.io</a> with the email address you
+        used to signup.</p>
+        <p>Thank you</p>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="success" dark
+          href="mailto:contact@etherstellar.io">
+          Send us an email
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>
 </template>
