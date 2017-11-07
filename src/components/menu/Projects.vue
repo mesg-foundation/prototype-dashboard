28302d769153b8f67448a29dfaad42db1a69c34b
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-11-07 17:32:27 +0700

    update project selection

--- src/components/menu/Projects.vue
+++ src/components/menu/Projects.vue
@@ -1,35 +1,33 @@
 <template>
-  <v-list>
-    <v-subheader class="grey--text">{{ $t('projects') }}</v-subheader>
-    <v-list-tile
-      router
-      :to="{ name: 'NewProject' }">
-      <v-list-tile-action>
-        <v-icon dark>add_circle_outline</v-icon>
-      </v-list-tile-action>
-      <v-list-tile-content>
-        <v-list-tile-title>
-          {{ $t('create') }}
-        </v-list-tile-title>
-      </v-list-tile-content>
-    </v-list-tile>
-    <v-list-tile
-      v-for="project in projects" :key="project.id"
-      :class="{ 'active': project.id === currentProjectId }"
-      @click.stop="changeProject({ project })">
-      <v-list-tile-content>
-        <v-list-tile-title>
-          {{ project.name }}
-        </v-list-tile-title>
-      </v-list-tile-content>
-    </v-list-tile>
-  </v-list>
+  <v-card>
+    <v-toolbar card>
+      <v-toolbar-title>{{ $t('title') }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" router :to="{ name: 'NewProject' }">
+        <v-icon class="mr-3">add_circle_outline</v-icon>
+        {{ $t('create') }}
+      </v-btn>
+    </v-toolbar>
+    <v-divider></v-divider>
+    <v-list two-line class="scrollable">
+      <v-list-tile
+        v-for="project in projects" :key="project.id"
+        :class="{ 'active': project.id === currentProjectId }"
+        @click.stop="changeProject({ project })">
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ project.name }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-card>
 </template>
 
 <i18n>
   en:
+    title: "Projects"
     create: "New Project"
-    projects: "PROJECTS"
 </i18n>
 
 <script>
